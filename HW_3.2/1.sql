204ff1fb370bf924413f09f171bdd3ec1cd47d16
Riba33 <78846229+Riba33@users.noreply.github.com>
2021-08-11 10:53:27 +0300

    Update 1.sql
    Убрал лишние кавычки, убрал название схемы - оставил только название таблицы.

--- HW_3.2/1.sql
+++ HW_3.2/1.sql
@@ -1,7 +1,7 @@
-ALTER TABLE `mydb`.`developers` 
-ADD COLUMN `salary` INT NOT NULL AFTER `gender`;
-UPDATE `mydb`.`developers` SET `salary` = '400' WHERE (`id` = '1');
-UPDATE `mydb`.`developers` SET `salary` = '500' WHERE (`id` = '2');
-UPDATE `mydb`.`developers` SET `salary` = '600' WHERE (`id` = '3');
-UPDATE `mydb`.`developers` SET `salary` = '450' WHERE (`id` = '4');
-UPDATE `mydb`.`developers` SET `salary` = '600' WHERE (`id` = '5');
+ALTER TABLE developers 
+ADD COLUMN salary INT NOT NULL AFTER gender;
+UPDATE developers SET salary = '400' WHERE (id = '1');
+UPDATE developers SET salary = '500' WHERE (id = '2');
+UPDATE developers SET salary = '600' WHERE (id = '3');
+UPDATE developers SET salary = '450' WHERE (id = '4');
+UPDATE developers SET salary = '600' WHERE (id = '5');
