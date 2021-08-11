fd64d20d52cbef47afdbfe7b1f19e30cc981dece
Riba33 <78846229+Riba33@users.noreply.github.com>
2021-08-11 13:34:18 +0300

    Update 2.sql
    Поправил имена, удалил замещение имен полей.

--- HW_3.2/2.sql
+++ HW_3.2/2.sql
@@ -1,10 +1,10 @@
 SELECT sum(salary) as cost,
-c.name as nameProject
-FROM mydb.developers as a
-INNER JOIN mydb.developers_projects as b
-ON a.id = b.developers_id
-INNER JOIN mydb.projects as c
-ON b.projects_id = c.id
-GROUP BY c.name 
-ORDER BY cost desc LIMIT 1
+	projects.name as projectName
+	FROM developers
+	INNER JOIN developers_projects
+	ON developers.id = developers_projects.developers_id
+	INNER JOIN projects
+	ON developers_projects.projects_id = projects.id
+	GROUP BY projects.name 
+	ORDER BY cost desc LIMIT 1
 ;
