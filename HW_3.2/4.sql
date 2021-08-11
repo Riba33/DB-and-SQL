60bda9fb95b5ba64cea817bd11d3dc9bb5c895fa
Riba33 <78846229+Riba33@users.noreply.github.com>
2021-08-11 13:44:47 +0300

    Update 4.sql
    Убрал ненужные кавычки кавычки.

--- HW_3.2/4.sql
+++ HW_3.2/4.sql
@@ -1,5 +1,6 @@
-ALTER TABLE `mydb`.`projects` 
-ADD COLUMN `cost` INT NOT NULL AFTER `name`;
-UPDATE `mydb`.`projects` SET `cost` = '5000' WHERE (`id` = '1');
-UPDATE `mydb`.`projects` SET `cost` = '6000' WHERE (`id` = '2');
-UPDATE `mydb`.`projects` SET `cost` = '7000' WHERE (`id` = '3');
+ALTER TABLE projects 
+	ADD COLUMN cost INT NOT NULL AFTER name
+;
+UPDATE projects SET cost = '5000' WHERE (id = '1');
+UPDATE projects SET cost = '6000' WHERE (id = '2');
+UPDATE projects SET cost = '7000' WHERE (id = '3');
