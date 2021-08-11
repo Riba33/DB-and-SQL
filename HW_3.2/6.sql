258b48012c589424c1ef634f67ded9bc9385cb25
Riba33 <78846229+Riba33@users.noreply.github.com>
2021-08-11 13:53:23 +0300

    Update 6.sql

--- HW_3.2/6.sql
+++ HW_3.2/6.sql
@@ -1,11 +1,11 @@
 SELECT avg(salary) as avgSalary,
-c.name as nameProject,
-avg(c.cost) as cost
-FROM mydb.developers as a
-INNER JOIN mydb.developers_projects as b
-ON a.id = b.developers_id
-INNER JOIN mydb.projects as c
-ON b.projects_id = c.id
-GROUP BY nameProject 
-ORDER BY cost Limit 1
+	projects.name as projectName,
+	avg(projects.cost) as cost
+	FROM developers
+	INNER JOIN developers_projects
+	ON developers.id = developers_projects.developers_id
+	INNER JOIN projects
+	ON developers_projects.projects_id = projects.id
+	GROUP BY projectName 
+	ORDER BY cost Limit 1
 ;
